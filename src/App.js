701d30c320f9579a7767e33f79e8b5df91401c96
SammuelJ <sammueljnssns@gmail.com>
2020-06-26 14:16:06 +0200

    cut into comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Game from "./Game";
+import Score from "./Score.js";
 
 function App() {
-    return <div className="App">Hello</div>;
+    return (
+        <div className="App">
+            <h1>Quick Math</h1>
+            <Game />
+            <Score />
+        </div>
+    );
 }
 
 export default App;
