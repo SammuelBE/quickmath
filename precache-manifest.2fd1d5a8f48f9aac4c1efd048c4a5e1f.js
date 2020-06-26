be6b5199d2dbfa40e310c670047d6c5e2a1cef02
SammuelJ <sammueljnssns@gmail.com>
2020-06-26 13:59:33 +0200

    Updates

---
+++ precache-manifest.2fd1d5a8f48f9aac4c1efd048c4a5e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a26fd7e1378091750325f67e70fc6e4",
+    "url": "/quickmatch/index.html"
+  },
+  {
+    "revision": "4cf587184ffc88eb97f3",
+    "url": "/quickmatch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4fea9532e6a963010132",
+    "url": "/quickmatch/static/js/2.db639809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quickmatch/static/js/2.db639809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf587184ffc88eb97f3",
+    "url": "/quickmatch/static/js/main.9b071e6b.chunk.js"
+  },
+  {
+    "revision": "8b677d97d91d143c9266",
+    "url": "/quickmatch/static/js/runtime-main.fcb98e46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quickmatch/static/media/logo.5d5d9eef.svg"
+  }
+]);
